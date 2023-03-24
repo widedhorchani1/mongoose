2322675d7da72e0ce6217dd4824e788e0507b975
widedhorchani1 <horchaniwided@gmail.com>
2023-03-24 14:13:12 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const express=require("express")
 const mongoose=require("mongoose")
 const creatPerson=require("./Models/persons")
-const wided= new creatPerson({name:'wided',age:29,favoriteFoods:['pizza','sushi']})
-console.log(wided)
-wided.save().then((savedWided) => {
-  savedWided === wided;
-});
 const app=express()
 mongoose
   .connect(
-    "mongodb+srv://horchaniwided:wided2023@cluster0.xhiylgq.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://horchaniwided:wided2023@cluster0.xhiylgq.mongodb.net/PersonContact?retryWrites=true&w=majority",
     { useNewUrlParser: true, useUnifiedTopology: true })
   .then((res) => {
     console.log("connection is susseful");
@@ -17,7 +12,45 @@ mongoose
   .catch((err) => {
     console.log("ther is an error",err);
   });
-  const document=mongoose.model
+  //create person
+
+//Person.create({name:'wided',age:29,favoriteFoods://['pizza','sushi']})
+
+//const person= new Person({name:'laila',age:29,favoriteFoods:['pizza','fastfood']})
+//person.save()
+
+// creat many person
+
+/*Person.insertMany([{
+    name:'Baha',
+    age:30,
+    favoriteFoods:['pasta','cookies']
+},
+{
+    name:'Asma',
+    age:26,
+    favoriteFoods:['cheesecake','takos']
+},
+{
+    name:'Maryam',
+    age:25,
+    favoriteFoods:['pasta','chiken']
+},
+])*/
+// find person
+
+/*console.log(Person.find().then((res)=>console.log(res)).catch((err)=>console.log(err)))*/ 
+
+/*console.log(Person.findById("641d91dce352fa13fe9dfd32").then((res)=>console.log(res)).catch((err)=>console.log(err)))*/
+//console.log(Person.findOne({name:'wided'}).exec()) 
+/*Person.findByIdAndUpdate("641d91dce352fa13fe9dfd32",{
+    $set:{
+        favoriteFoods:favoriteFoods.push("hamburger","pizza"),
+    }
+}).then((res)=>console.log(res)).catch((err)=>console.log(err))*/
+
+Person.findByIdAndDelete("641d953b53d472d1abf7634a").then((res)=>console.log(res)).catch((err)=>console.log(err))
+
 app.listen(5000,(err)=>{
     if (err)throw err;
     console.log("sever is up and runnign")
